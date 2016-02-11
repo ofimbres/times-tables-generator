9806056ff89fbfd70dd877ead6e1eb99024b19cf
unknown <oscar.fimbres@gmail.com>
2016-02-10 22:16:06 -0600

    Added a conditional into the 'answerSelector' to decide if choose add1ToFactor or substract1ToFactor

--- app/answerSelector.js
+++ app/answerSelector.js
@@ -2,10 +2,10 @@ var generators = require('./answer-generators'),
 	utils = require('./utils');
 
 // even?
-// if rightAnswer.length >=2 
+// if rightAnswer.length >=2
 // .. then Inverted number
 // .. else random number
-// Multiple -1 
+// Multiple -1
 // Right answer
 // Multiple +1
 function answerSelector(problem) {
@@ -17,8 +17,11 @@ function answerSelector(problem) {
 		generators.findRandomNumber1(problem, currentAnswersArray);
 	}
 
-	generators.add1ToYFactor(problem, currentAnswersArray);
-	generators.substract1ToYFactor(problem, currentAnswersArray);
+	if (Math.random() > 0.5) {
+		generators.add1ToYFactor(problem, currentAnswersArray);
+	} else {
+		generators.substract1ToYFactor(problem, currentAnswersArray);
+	}
 
 	// Last phase
 	while(currentAnswersArray.length < 4) {
